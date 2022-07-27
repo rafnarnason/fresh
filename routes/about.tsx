f86a41807f3cabca8895b58db6baffba8bdc6872
Rafn Árnason <rafnarnason@gmail.com>
2022-07-27 15:48:29 +0000

    update about

--- routes/about.tsx
+++ routes/about.tsx
@@ -6,8 +6,8 @@ import { h } from "preact";
 export default function AboutPage() {
   return (
     <main>
-      <h1>About</h1>
-      <p>This is the about page.</p>
+      <h1>About Page</h1>
+      <p>This is the about page. #######################</p>
     </main>
   );
 }
